335111129f636f9396e0f3caa9d71b0f2025f655
Esseson <Essesonahmed@yahoo.com>
2019-03-13 08:15:32 -0700

    update web developer
    trying

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -65,7 +65,7 @@ const Index = () => (
         <BigTitle>
           Hello, <br /> I'm Esseson.
         </BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle>I'm a student, web developer and an Aspiring entreprenuer.</Subtitle>
       </Hero>
       <Projects offset={1}>
         <Title>Projects</Title>
