280d6b2ed696e4140de06fd00b5a95786787dead
Esseson <35272146+Esseson@users.noreply.github.com>
2019-10-25 05:52:00 -0700

    adding website changes

--- config/website.js
+++ config/website.js
@@ -7,15 +7,15 @@ module.exports = {
   siteTitleAlt: 'Esseson', // Alternative Site title for SEO
   siteTitleShort: 'Esseson', // short_name for manifest
   siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://esseson.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
   siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
   author: 'LekoArts', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
+  userTwitter: '@esseson', // Twitter Username
+  ogSiteName: 'Esseson Peniel Raphael', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
   googleAnalyticsID: 'UA-47519312-5',
 
