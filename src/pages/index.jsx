1dbcdc014e2d487c380a64a9a94499f4ade1c3db
Esseson <35272146+Esseson@users.noreply.github.com>
2020-03-20 08:28:09 -0700

    Adding Socials

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -148,6 +148,14 @@ const Index = () => (
           <ContactText>
             Say <a href = "mailto: iamesseson@gmail.com"> Hi</a> or find me on other platforms:{' '}
             <a href="tel:1-540-360-1924">1-540-360-1924</a>
+            <a href="https://www.linkedin.com/in/raphael-esseson-b6940212a/">LinkedIn</a>
+            <a href="https://www.instagram.com/esseson/">Instagram</a>
+            <a href="https://web.facebook.com/Lord.Esseson">Facebook</a>
+            <a href="https://esseson.contently.com/">Contently</a>
+
+
+
+
           </ContactText>
         </Inner>
         <Footer>
