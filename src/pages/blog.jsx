bcb4f941b77fb71f1b7c85c30daa57d7f1cf92fe
Esseson <35272146+Esseson@users.noreply.github.com>
2020-07-22 00:19:22 -0700

    blog page

---
+++ src/pages/blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Blog = () => (
+    
+    <div>
+      <h1>Welcome To My Blog page</h1>
+    </div>
+  );
+  
+  export default Blog;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import tw from 'tailwind.macro'
+import link from 'gatsby'
 import { Parallax } from 'react-spring/renderprops-addons.cjs'
 
 // Components
